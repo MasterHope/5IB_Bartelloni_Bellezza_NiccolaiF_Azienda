3a612d678e397be1c933162f4d22c5136cf4f18e
MasterHope <bellezza.francesco@studenti-ittfedifermi.gov.it>
2019-05-14 22:02:59 +0200

    Modifica Dao

--- Dao/AcquistiDao.php
+++ Dao/AcquistiDao.php
@@ -1,10 +1,34 @@
 <?php
 require_once'Dao.php';
 /**
- * Description of AcquistiDao
+ * Classe utilizzata per la gestione dei dati inerenti agli acquisti.
  *
- * @author utente
+ * @author Bartelloni-Bellezza-NiccolaiF
  */
-class AcquistiDao {
-    //put your code here
+class AcquistiDao extends Dao{
+    /**
+     * Metodo che consente di modificare la data di spedizione di un acquisto.
+     * @param string $codice Codice dell'acquisto
+     * @param string $data_spedizione Data dell'effettiva spedizione
+     * @return int 0 se la quantità non è stata aggiornata, -1 se la query
+     * non è stata eseguita, 1 se è stata eseguita correttamente.
+     */
+    function modificaSpedizione($codice,$data_spedizione){
+        $ok=1;
+        $sql="update Acquisti set data_spedizione=? where codice_acquisto=?";
+        $connection=parent::getConnection();
+        $st=$connection->prepare($sql);
+        $st->bind_param("ss", $data_spedizione,$codice);
+        if(!$st->execute()){
+            $ok=-1;
+        } else {
+            if($st->affected_rows==0){
+                $ok=0;
+            }
+        }
+        $st->close();
+        $connection->close();
+        return $ok;
+    }
+    
 }
--- Dao/ProdottiDao.php
+++ Dao/ProdottiDao.php
@@ -1,31 +1,95 @@
 <?php
+
 require_once'Dao.php';
 require_once'bean/Prodotto.php';
+
 /**
  * Classe Dao utilizzata per gestire i prodotti presenti nella base di dati.
  *
  * @author Bartelloni-Bellezza-NiccolaiF
  */
-class ProdottiDao extends Dao{
+class ProdottiDao extends Dao {
+
     /**
      * Metodo che ritorna tutti i prodotti presenti nel database.
      * @return \Prodotto Array di prodotti presenti nella base di dati.
      */
-    function findAll(){
-        $prodotti=array();
-        $connection=parent::getConnection();
-        $sql="select * from Prodotti";
-        $result=$connection->query($sql);
-        while($row = $result->fetch_array()){
-            $codice_prodotto=$row['codice_prodotto'];
-            $denominazione=$row['denominazione'];
-            $descrizione=$row['descrizione'];
-            $prezzo=$row['prezzo'];
-            $quantita=$row['quantita'];
-            $prodotto=new Prodotto($codice_prodotto,$denominazione,
-                    $descrizione,$prezzo, $quantita);
-            $prodotti[]=$prodotto;
+    function findAll() {
+        $prodotti = array();
+        $connection = parent::getConnection();
+        $sql = "select * from Prodotti";
+        $result = $connection->query($sql);
+        while ($row = $result->fetch_array()) {
+            $codice_prodotto = $row['codice_prodotto'];
+            $denominazione = $row['denominazione'];
+            $descrizione = $row['descrizione'];
+            $prezzo = $row['prezzo'];
+            $quantita = $row['quantita'];
+            $prodotto = new Prodotto($codice_prodotto, $denominazione, $descrizione, $prezzo, $quantita);
+            $prodotti[] = $prodotto;
         }
+        $result->free();
+        $connection->close();
         return $prodotti;
+    }
+
+    /**
+     * Metodo per l'aggiornamento della quantità dei prodotti.
+     * @param int $quantita Quantità dei prodotti nuova del database.
+     * @return int 0 se la quantità non è stata aggiornata, -1 se la query
+     * non è stata eseguita, 1 se è stata eseguita correttamente.
+     */
+    function aggiornaQuantita($codice, $quantita) {
+        $ok = 1;
+        $sql = "update Prodotti set quantita=? where codice_prodotto=?";
+        $connection = parent::getConnection();
+        $st = $connection->prepare(sql);
+        $st->bind_param("is", $codice, $quantita);
+        if (!$st->execute()) {
+            $ok = -1;
+        } else {
+            if ($st->affected_rows == 0) {
+                $ok = 0;
+            }
+        }
+        $st->close();
+        $connection->close();
+        return $ok;
+    }
+
+    /**
+     * Metodo utilizzato per inserire un nuovo prodotto.
+     * @param Prodotto $prodotto prodotto Inserito nel database
+     * @return int 0 se la quantità non è stata aggiornata, -1 se la query
+     * non è stata eseguita, 1 se è stata eseguita correttamente.
+     */
+    function inserisciProdotto($prodotto) {
+        $ok = 1;
+        $sql = "insert into Prodotti values(?,?,?,?,?)";
+        $connection = parent::getConnection();
+        if (!prodottoEsiste($prodotto)) {
+            $st = $connection->prepare($sql);
+            $codice_prodotto = $prodotto->getCodice_prodotto();
+            $denominazione = $prodotto->getDenominazione();
+            $descrizione = $prodotto->getDescrizione();
+            $prezzo = $prodotto->getPrezzo();
+            $quantita = $prodotto->getQuantita();
+            $st->bind_param("sssdi", $codice_prodotto, $denominazione, $descrizione, $prezzo, $quantita);
+            if (!$st->execute()) {
+                $ok = -1;
+            }
+        } else {
+            $ok = 0;
+        }
+        $st->close();
+        $connection->close();
+        return $ok;
+    }
+    /**
+     * Metodo utilizzato per verificare se il prodotto esiste o meno nel database. 
+     * Ritorna bool True se esiste, false altrimenti.
+     */
+    private function prodottoEsiste(){
+        
     }
 }
