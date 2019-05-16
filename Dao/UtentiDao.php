7ab3024a40019b8877730b4f3b991830246ec7e6
hirotakatech <hirotakatech@gmail.com>
2019-05-16 17:08:47 +0200

    Aggiunto metodo per inserimento utente

--- Dao/UtentiDao.php
+++ Dao/UtentiDao.php
@@ -2,10 +2,36 @@
 
 require_once'Dao.php';
 /**
- * Description of UtentiDao
+ * Classe che rappresenta un utente
  *
- * @author utente
+ * @author Bartelloni-Bellezza-NiccolaiF
  */
-class UtentiDao {
-    //put your code here
+class UtentiDao extends Dao{
+	public function insert($utente,$password,$ruolo){
+		if($this->exists($utente)){
+			$password=crypt($password,0);
+			$sql="insert into Utenti values(?,?,?,?)";
+			$con=parent::getConnection();
+			$st=$con->prepare($sql);
+			$st->bind_param("sssi", $codiceUtente,$utente,$password,$idRuolo);
+			if($st->execute()){
+				return true;
+			}else{
+				throw new Exception("Query non andata a buon fine", $code, $previous);
+			}
+		}
+
+
+	}
+	public function exists($utente){
+		$sql="select * from Utenti where Utenti.username=?";
+		$con=parent::getConnection();
+		$st=$con->prepare($sql);
+		$st->bind_param("s", $utente);
+		return $st->execute();
+
+	}
+
+
+	
 }
