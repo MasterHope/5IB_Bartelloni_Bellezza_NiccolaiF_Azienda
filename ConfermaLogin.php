0f37f4ee574313a2b1b6d916247d570f26536243
hirotakatech <hirotakatech@gmail.com>
2019-05-22 20:16:46 +0200

    Aggiunto gitignoer

--- ConfermaLogin.php
+++ ConfermaLogin.php
@@ -1,32 +1,32 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>ConfermaLogin</title>
-	</head>
-	<body>
 <?php
-
+session_start();
+define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT']);
+session_unset();
+session_destroy();
 require_once 'Dao/UtentiDao.php';
 $user = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $utenti=new UtentiDao();
 if($utenti->checkLogin($user,$password)){
 	session_start();
-	session_unset();
-	session_destroy();
-	session_start();
 	$_SESSION['utente']=$user;
 	$_SESSION['password']=md5($password);
+	die;
 }
-
-
-
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once 'head.php' ?>
+		<meta charset="UTF-8">
+		<title>ConfermaLogin</title>
+	</head>
+	<body>
+		<?php include_once 'header.php' ?>
 <div style="margin-top:150px">
 	<?php echo $_SESSION['utente']; ?>
 
 </div>
-
+		<?php include_once 'footer.php' ?>
 	</body>
 </html>
--- session.php
+++ session.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
 if (!isset($_SESSION['username'])) {
-	header("Location:".$_SERVER['workdir']."/Login.php");
+	header("Location:".$_SERVER['DOCUMENT_ROOT']."/Login.php");
 	die;
 }
