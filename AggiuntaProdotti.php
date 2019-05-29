b0bb437999bb3698e131488841b933b60116da42
hirotakatech <hirotakatech@gmail.com>
2019-05-29 13:33:24 +0200

    Modificato metodo Get

--- AggiuntaProdotti.php
+++ AggiuntaProdotti.php
@@ -23,25 +23,25 @@ $listaProdotti = $prodotti->findAll();
     <body>
 	<?php include_once 'header.php'; ?>
 	<div class="container  col-10 justify-content-center" style="margin-top: 150px">
-	    <form action="ConfermaAggiuntaProdotto.php">
+		<form action="ConfermaAggiuntaProdotto.php" method="POST">
 		<div class="row">
 		    <div class="col-md-4">
 			<h3>Denominazione</h3>
-			<input name="denominazione" type="text">
+			<input name="denominazione" type="text" required>
 		    </div>
 		    <div class="col-md-4">
 			<h3>Prezzo</h3>
-			<input name="prezzo" type="number">
+			<input name="prezzo" type="number" min="1"  max="9999" required>
 		    </div>
 		    <div class="col-md-4">
 			<h3>Quantita</h3>
-			<input name="quantita" type="number">
+			<input name="quantita" type="number" min="1" step="1" max="100" required>
 		    </div>
 		</div>
 		<div class="row" style="margin-top: 10px">
 		    <center>
 			<div class="col-md-12">
-			    <textarea style="col-md-12" name="descrizione"></textarea>
+			    <textarea style="col-md-12" name="descrizione" required></textarea>
 		    </div>
 		    </center>
 		</div>
