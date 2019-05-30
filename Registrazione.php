a813f0420fb52522d102005d0784c0614a92e332
MasterHope <bellezza.francesco@studenti-ittfedifermi.gov.it>
2019-05-30 16:14:20 +0200

    Controlli aggiunti in registrazione.

--- Registrazione.php
+++ Registrazione.php
@@ -16,41 +16,43 @@
 		<div class="row align-items-center" >
 		    <div class="col-md-6">
 			<h2>Username</h2>
-			<input type="text" name="username" value="" required="true"/>
+			<input type="text" name="username" value="" required="true" minlength="4" maxlength="40"/>
 		    </div>
 		    <div class="col-md-6">
 			<h2>Password</h2>
-			<input type="password" name="password" value="" required="true"/>
+                        <input type="password" name="password" value="" required="true" minlength="8" maxlength="40"/>
 		    </div>
 		</div>
                 <div class="row">
                     <div class="col-md-6">
                         <h3>Nome</h3>
-                        <input type="text" name="nome" value="" required/>
+                        <input type="text" name="nome" value="" required minlength="4" maxlength="40"/>
                     </div>
                     <div class="col-md-6">
                         <h3>Cognome</h3>
-                        <input type="text" name="cognome" value="" required/>
+                        <input type="text" name="cognome" value="" required minlength="4" maxlength="40"/>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <h3>Indirizzo</h3>
-                        <input type="text" name="indirizzo" value="" required/>
+                        <input type="text" name="indirizzo" value="" required minlength="4" maxlength="40"/>
                     </div>
                     <div class="col-md-6">
                         <h3>Città</h3>
-                        <input type="text" name="citta" value="" required/>
+                        <input type="text" name="citta" value="" required minlength="4" maxlength="40"/>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <h3>Cellulare</h3>
-                        <input type="text" name="telefono" pattern="\d{10}" value="" required/>
+                        <input type="text" name="telefono" pattern="\d{10}" 
+                               title="Il numero di telefono deve contenere 10 cifre" value="" required/>
                     </div>
                     <div class="col-md-6">
                         <h3>CAP</h3>
-                        <input type="text" name="cap" value="" pattern="\d{5}" required/>
+                        <input type="text" name="cap" value="" pattern="\d{5}" 
+                               title="Il CAP è composto obbligatoriamente da 5 cifre" required/>
                     </div>
                 </div>
                 
