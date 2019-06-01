a1d0f84abbb95cd6153143cddfef1f4d5fb53f41
hirotakatech <hirotakatech@gmail.com>
2019-06-01 19:22:36 +0200

    Rimossi file inutili e modificati commenti

--- isResponsabileMarketing.php
+++ isResponsabileMarketing.php
@@ -1,5 +1,5 @@
 <?php
-//Questo file viene utilizzato per verificare se una determinata persona è un cliente o meno.
+//Questo file viene utilizzato per verificare se una determinata persona è un responsabile del marketing o meno.
 require_once"Dao/UtentiDao.php";
 if(!isset($_SESSION)){
     session_start();
